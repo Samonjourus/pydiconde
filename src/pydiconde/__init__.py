d8bd344eaabc58a1a48f59cb6bdce4b139722465
Benjamin <benjaminstandfield@gmail.com>
2025-03-28 14:54:19 -0400

    feat(Diconde): Added general DICONDE class

---
+++ src/pydiconde/__init__.py
@@ -0,0 +1,12 @@
+from modules.component import DICONDEComponent
+from modules.componentseries import DICONDEComponentSeries
+from modules.componentstudy import DICONDEComponentStudy
+from modules.ndeequipment import DICONDENDEquipment
+from modules.ndegeometry import DICONDENDEGeometry
+from modules.ndeindication import DICONDENDEIndication
+
+from pydicom.dataset import FileMetaDataset
+
+class Diconde(DICONDEComponent, DICONDEComponentSeries, DICONDEComponentStudy, DICONDENDEquipment, DICONDENDEGeometry, DICONDENDEIndication):
+    def __init__(self, file_path, object, file_meta=FileMetaDataset()):
+        super().__init__(file_path, object, file_meta=file_meta)
