7f4fd12413c7c333f4c0c0ece5ceff7dc2103552
Benjamin Standfield <benjaminstandfield@gmail.com>
2025-04-02 10:54:00 -0400

    fix: moved attributes in component module
    According to Andrew Ferro who serves as chair on the DICONDE working group, there
    is a misprint on the version 21.

--- src/pydiconde/modules/component.py
+++ src/pydiconde/modules/component.py
@@ -29,81 +29,9 @@ class otherComponentIDsSequenceElement(Dataset):
         """
         return self[Tag(0x0010, 0x1001)].value
 
-    @property
-    def componentManufacturingDate(self) -> datetime:
-        """ The component manufacturing date to be assigned to tag (0010,0030).
-
-        The value is expected to be a date. The value is required, but can be zero-valued.
-        """
-        return self[Tag(0x0010, 0x0030)].value
-
-    @property
-    def patientSex(self) -> str:
-        """ The patient sex to be assigned to tag (0010,0040).
-
-        The value is required to be a code string, but should be set to 'O'.
-        """
-        return self[Tag(0x0010, 0x0040)].value
-
-    @property
-    def componentNotes(self) -> str:
-        """ The component notes to be assigned to tag (0010,4000).
-
-        This field is optional, but if defined, should be a long text.
-        """
-        return self[Tag(0x0010, 0x4000)].value
-
-    @property
-    def componentManufacturingProcedure(self) -> str:
-        """ The component manufacturing procedure to be assigned to tag (0014,0025).
-
-        This field is optional, but if defined, should be a short text.
-        """
-        return self[Tag(0x0014, 0x0025)].value
-
-    @property
-    def componentManufacturer(self) -> str:
-        """ The component manufacturer to be assigned to tag (0014,0028).
-
-        This field is optional, but if defined, should be a short text.
-        """
-        return self[Tag(0x0014,0x0028)].value
-
-    @property
-    def componentWelderIDs(self) -> str:
-        """ The component welder IDs to be assigned to tag (0014,0100).
-
-        This field is optional, but if defined, should be long text.
-        """
-        return self[Tag(0x0014,0x0100)].value
-
     @otherComponentNames.setter
     def otherComponentNames(self, value: list[str]):
         self.add_new(Tag(0x0010, 0x1001), "PN", value)
-
-    @componentManufacturingDate.setter
-    def componentManufacturingDate(self, value: datetime):
-        self.add_new(Tag(0x0010, 0x0030), "DA", value)
-
-    @patientSex.setter
-    def patientSex(self, value: str):
-        self.add_new(Tag(0x0010, 0x0040), "CS", value)
-
-    @componentNotes.setter
-    def componentNotes(self, value: str):
-        self.add_new(Tag(0x0010, 0x4000), "LT", value)
-
-    @componentManufacturingProcedure.setter
-    def componentManufacturingProcedure(self, value: str):
-        self.add_new(Tag(0x0014, 0x0025), "ST", value)
-
-    @componentWelderIDs.setter
-    def componentWelderIDs(self, value: str):
-        self.add_new(Tag(0x0014, 0x0100), "ST", value)
-
-    @componentManufacturer.setter
-    def componentManufacturer(self, value: str):
-        self.add_new(Tag(0x0014, 0x0028), "ST", value)
 
 class DICONDEComponent(FileDataset):
     def __init__(self, file_path, object, file_meta=FileMetaDataset()):
@@ -207,6 +135,54 @@ class DICONDEComponent(FileDataset):
         """
         return self[Tag(0x0014,0x0056)].value
 
+    @property
+    def componentManufacturingDate(self) -> datetime | None:
+        """ The component manufacturing date to be assigned to tag (0010,0030).
+
+        The value is expected to be a date. The value is required, but can be zero-valued.
+        """
+        return self[Tag(0x0010, 0x0030)].value
+
+    @property
+    def patientSex(self) -> str | None:
+        """ The patient sex to be assigned to tag (0010,0040).
+
+        The value is required to be a code string, but should be set to 'O'.
+        """
+        return self[Tag(0x0010, 0x0040)].value
+
+    @property
+    def componentNotes(self) -> str | None:
+        """ The component notes to be assigned to tag (0010,4000).
+
+        This field is optional, but if defined, should be a long text.
+        """
+        return self[Tag(0x0010, 0x4000)].value
+
+    @property
+    def componentManufacturingProcedure(self) -> str | None:
+        """ The component manufacturing procedure to be assigned to tag (0014,0025).
+
+        This field is optional, but if defined, should be a short text.
+        """
+        return self[Tag(0x0014, 0x0025)].value
+
+    @property
+    def componentManufacturer(self) -> str | None:
+        """ The component manufacturer to be assigned to tag (0014,0028).
+
+        This field is optional, but if defined, should be a short text.
+        """
+        return self[Tag(0x0014,0x0028)].value
+
+    @property
+    def componentWelderIDs(self) -> str | None:
+        """ The component welder IDs to be assigned to tag (0014,0100).
+
+        This field is optional, but if defined, should be long text.
+        """
+        return self[Tag(0x0014,0x0100)].value
+
     @materialName.setter
     def materialName(self, value: str):
         self.add_new(Tag(0x0014,0x2160), "SH", value)
@@ -259,3 +235,26 @@ class DICONDEComponent(FileDataset):
     def componentIDNumber(self, value: str):
         self.add_new(Tag(0x0010, 0x0020), "LO", value)
 
+    @componentManufacturingDate.setter
+    def componentManufacturingDate(self, value: datetime | None):
+        self.add_new(Tag(0x0010, 0x0030), "DA", value)
+
+    @patientSex.setter
+    def patientSex(self, value: str | None):
+        self.add_new(Tag(0x0010, 0x0040), "CS", value)
+
+    @componentNotes.setter
+    def componentNotes(self, value: str | None):
+        self.add_new(Tag(0x0010, 0x4000), "LT", value)
+
+    @componentManufacturingProcedure.setter
+    def componentManufacturingProcedure(self, value: str | None):
+        self.add_new(Tag(0x0014, 0x0025), "ST", value)
+
+    @componentWelderIDs.setter
+    def componentWelderIDs(self, value: str | None):
+        self.add_new(Tag(0x0014, 0x0100), "ST", value)
+
+    @componentManufacturer.setter
+    def componentManufacturer(self, value: str | None):
+        self.add_new(Tag(0x0014, 0x0028), "ST", value)
