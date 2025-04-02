cf933a989d019d49f3a043e16c162896af5cf9d7
Benjamin Standfield <benjaminstandfield@gmail.com>
2025-04-02 10:52:11 -0400

    fix: dixed module imports from root __init__

--- src/pydiconde/__init__.py
+++ src/pydiconde/__init__.py
@@ -1,9 +1,9 @@
-from modules.component import DICONDEComponent
-from modules.componentseries import DICONDEComponentSeries
-from modules.componentstudy import DICONDEComponentStudy
-from modules.ndeequipment import DICONDENDEquipment
-from modules.ndegeometry import DICONDENDEGeometry
-from modules.ndeindication import DICONDENDEIndication
+from .modules.component import DICONDEComponent
+from .modules.componentseries import DICONDEComponentSeries
+from .modules.componentstudy import DICONDEComponentStudy
+from .modules.ndeequipment import DICONDENDEquipment
+from .modules.ndegeometry import DICONDENDEGeometry
+from .modules.ndeindication import DICONDENDEIndication
 
 from pydicom.dataset import FileMetaDataset
 
