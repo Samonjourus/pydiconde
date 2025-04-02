537eadee723afb5e338a932f36763cf7cf96f597
Benjamin Standfield <benjaminstandfield@gmail.com>
2025-04-02 13:49:10 -0400

    chore: updated example to split dataset and filedataset

--- example/minimal.py
+++ example/minimal.py
@@ -6,11 +6,24 @@ fields are ignored.
 from pydiconde import Diconde
 from datetime import datetime, timezone
 from pydicom import config
+from pydicom.dataset import FileMetaDataset
+import numpy as np
+from pydicom.uid import UID, ExplicitVRLittleEndian
 
-config.INVALID_KEYWORD_BEHAVIOR = "IGNORE"
+config.INVALID_KEYWORD_BEHAVIOR = "IGNORE" # we're adding non-DICOM fields
+image_data = np.uint16(np.random.rand(128, 128) * 65535) # sample image data
 
 # create diconde file
-diconde_file = Diconde("./minimal.diconde", {})
+diconde_file = Diconde()
+
+# necessary file meta data information
+file_meta = FileMetaDataset()
+file_meta.MediaStorageSOPClassUID = UID("1.2.840.10008.5.1.4.1.1.2")
+file_meta.MediaStorageSOPInstanceUID = UID("1.2.3")
+file_meta.ImplementationClassUID = UID("1.2.3.4")
+file_meta.TransferSyntaxUID = ExplicitVRLittleEndian
+
+diconde_file.file_meta = file_meta
 
 # fill out the component module
 diconde_file.componentName = ""
@@ -43,5 +56,14 @@ diconde_file.manufacturer = ""
 
 # Nothing else is required for a compliant DICONDE file
 
+# Save an image
+diconde_file.BitsAllocated = 16
+diconde_file.BitsStored = 16
+diconde_file.HighBit = 15
+diconde_file.SamplesPerPixel = 1
+diconde_file.PixelRepresentation = 0
+diconde_file.PixelData = image_data.tobytes()
+
 # save to disk
-diconde_file.save_as("./minimal.diconde")
+diconde_file.save_as("./minimal.dcm", enforce_file_format=True)
+print(diconde_file)
