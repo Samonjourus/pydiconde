95ca1261aad7f3a515fe67701c3625b77d91a0b7
Benjamin Standfield <benjaminstandfield@gmail.com>
2025-04-02 13:43:31 -0400

    feat: added method for replacing DICOM names with DICONDE

--- src/pydiconde/__init__.py
+++ src/pydiconde/__init__.py
@@ -4,7 +4,17 @@ from .modules.componentstudy import DICONDEComponentStudy
 from .modules.ndeequipment import DICONDENDEquipment
 from .modules.ndegeometry import DICONDENDEGeometry
 from .modules.ndeindication import DICONDENDEIndication
+from pydicom.datadict import add_dict_entries
 
 class Diconde(DICONDEComponent, DICONDEComponentSeries, DICONDEComponentStudy, DICONDENDEquipment, DICONDENDEGeometry, DICONDENDEIndication):
     def __init__(self):
         super().__init__()
+
+    @staticmethod
+    def use_diconde_names():
+        """Use Diconde21 names for addresses"""
+        entries = {
+            0x00142160: ("SH", "1", "Material Name", "", "MaterialName"),
+            0x00081048: ("PN", "1", "Inspecting Company Name", "", "InspectingCompanyName")
+        }
+        add_dict_entries(entries)
